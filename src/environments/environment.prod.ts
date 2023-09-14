9d008c5661fae76498e8dd38242d715d6a7821a1
Charl <charl@nclose.co.za>
2023-09-14 15:50:37 +0200

    Bug fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     title: 'Production Environment',
-    apiURL: 'https://invoice-api-31fabc85cd9b.herokuapp.com/'
+    apiUrl: 'https://invoice-api-31fabc85cd9b.herokuapp.com/'
 };
